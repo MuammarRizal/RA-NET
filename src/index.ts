d14122cca8f1e926f570e9d80dbfe2dcaa24f331
MuammarRizal <muammarrizal31@gmail.com>
2024-09-11 21:40:34 +0700

    setup projects #1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-function da() {
-  console.log("Helloworld");
-}
+import express, { Application, NextFunction, Request, Response } from "express";
 
-da();
+const app: Application = express();
+const PORT: Number = 4000;
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: "Jange" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port : ${PORT}`);
+});
