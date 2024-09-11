b456b62bdf2b1df49cc3f59c09cfd52563c4fce1
MuammarRizal <muammarrizal31@gmail.com>
2024-09-12 02:22:57 +0700

    Handler request

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import Express, { Application } from 'express'
+import Express, { Application, NextFunction, Request, Response } from 'express'
 import { routes } from './routes'
 import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 const app: Application = Express()
 const PORT: Number = 4000
 
+// parse body request
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// cors access handler
+app.use(cors())
+app.use((req:Request,res:Response,next:NextFunction) => { // Memberikan control ketika ingin mengakses REST API
+  res.setHeader("Access-Control-Allow-Origin","*") // Allowed url any to access
+  res.setHeader("Access-Control-Allow-Methods","*") // Allowrd methods anything to access
+  res.setHeader("Access-Control-Allow-Headers","*") // Given headers anything to access
+  next()
+})
+
+// Routes for to access
 routes(app)
 
 app.listen(PORT, () => {
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -7,3 +7,14 @@ ProductsRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('Get Products success')
   res.status(200).send({ products: [{ product: 'products-1' }] })
 })
+
+ProductsRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({
+    status: true,
+    statusCode: 200,
+    data: {
+      id: +new Date(),
+      ...req.body
+    }
+  })
+})
