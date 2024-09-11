93770c29aca1d299e698c1e2ef1be5922ed10356
MuammarRizal <muammarrizal31@gmail.com>
2024-09-12 00:08:37 +0700

    setup #2 eslint & prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from 'express'
 
-const app: Application = express();
-const PORT: Number = 4000;
+const app: Application = express()
+const PORT: Number = 4000
 
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: "Jange" });
-});
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: 'Jange' })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at port : ${PORT}`);
-});
+  console.log(`Server is listening at port : ${PORT}`)
+})
