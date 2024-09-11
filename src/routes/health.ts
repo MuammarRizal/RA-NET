a7715fab105f5185c864fc2ed25ee1d4f228277e
MuammarRizal <muammarrizal31@gmail.com>
2024-09-12 01:39:47 +0700

    setup router

---
+++ src/routes/health.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response, Router } from 'express'
+
+export const HealthRouter: Router = Router()
+
+HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({
+    status: true,
+    statusCode: 200,
+    data: [
+      { nama: 'Muammar Rizal', npm: '202043502004' },
+      { nama: 'Alfina Rahmalia', npm: '202043502001' },
+      { nama: 'Adlian simatupang', npm: '202043502002' }
+    ]
+  })
+})
